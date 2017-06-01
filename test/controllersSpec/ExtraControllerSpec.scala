01975f5d7c55a9c39f0e0ba25e04f40353bb061c
felixthomas <felix.thomas@web.de>
2017-06-01 17:56:25 +0200

    Specs2 Tests für ExtraController erweitert um weitere Bad_Requests und die view-Aufrufe

--- test/controllersSpec/ExtraControllerSpec.scala
+++ test/controllersSpec/ExtraControllerSpec.scala
@@ -7,6 +7,7 @@ import org.specs2.runner._
 import play.api.test.Helpers._
 import play.api.test._
 import controllers.ExtraController
+import dbaccess.ExtraDao
 
 import scala.None
 
@@ -24,7 +25,7 @@ class ExtraControllerSpec extends Specification{
 
   "ExtraController" should {
 
-    "successfully add an Extra" in memDB {
+    "add an Extra" in memDB {
       val request = FakeRequest(POST, "/addExtra").withFormUrlEncodedBody(
         "id" -> "0",
         "Name" -> "Kas",
@@ -51,17 +52,29 @@ class ExtraControllerSpec extends Specification{
         "Name" -> "Kas",
         "Preis" -> "1"
       )
-      val result = ExtraController.addExtra()(request)
-      status(result) must equalTo(SEE_OTHER)
-      redirectLocation(result) must beSome("/newExtraCreated?id=-1&name=Kas&price=1.0")
       val request2 = FakeRequest(POST, "/updateExtra").withFormUrlEncodedBody(
         "id" -> "0",
         "Name" -> "Speck",
         "Preis" -> "1"
       )
-      val result2 = ExtraController.updateExtra()(request2)
-      status(result2) must equalTo(SEE_OTHER)
-      redirectLocation(result2) must beSome("/upgradeExtra?id=0&name=Speck&price=1.0")
+      val result = ExtraController.updateExtra()(request2)
+      status(result) must equalTo(SEE_OTHER)
+      redirectLocation(result) must beSome("/upgradeExtra?id=0&name=Speck&price=1.0")
+    }
+
+    "update an Extra bad request" in memDB {
+      val request = FakeRequest(POST, "/addExtra").withFormUrlEncodedBody(
+        "id" -> "0",
+        "Name" -> "Kas",
+        "Preis" -> "1"
+      )
+      val request2 = FakeRequest(POST, "/updateExtra").withFormUrlEncodedBody(
+        "Name" -> "Speck",
+        "Preis" -> "1"
+      )
+      val result = ExtraController.updateExtra()(request2)
+      status(result) must equalTo(BAD_REQUEST)
+      redirectLocation(result) must beNone
     }
 
     "remove an Extra" in memDB {
@@ -70,15 +83,26 @@ class ExtraControllerSpec extends Specification{
         "Name" -> "Kas",
         "Preis" -> "1"
       )
-      val result = ExtraController.addExtra()(request)
-      status(result) must equalTo(SEE_OTHER)
-      redirectLocation(result) must beSome("/newExtraCreated?id=-1&name=Kas&price=1.0")
       val request2 = FakeRequest(POST, "/rmExtra").withFormUrlEncodedBody(
         "id" -> "0"
       )
-      val result2 = ExtraController.rmExtra()(request2)
-      status(result2) must equalTo(SEE_OTHER)
-      redirectLocation(result2) must beSome("/extraDeleted?deleted=true")
+      val result = ExtraController.rmExtra()(request2)
+      status(result) must equalTo(SEE_OTHER)
+      redirectLocation(result) must beSome("/extraDeleted?deleted=true")
+    }
+
+    "remove an Extra bad request" in memDB {
+      val request = FakeRequest(POST, "/addExtra").withFormUrlEncodedBody(
+        "id" -> "0",
+        "Name" -> "Kas",
+        "Preis" -> "1"
+      )
+      val request2 = FakeRequest(POST, "/rmExtra").withFormUrlEncodedBody(
+        "ID" -> "0"
+      )
+      val result = ExtraController.rmExtra()(request2)
+      status(result) must equalTo(BAD_REQUEST)
+      redirectLocation(result) must beNone
     }
 
     "get an Extra" in memDB {
@@ -87,15 +111,70 @@ class ExtraControllerSpec extends Specification{
         "Name" -> "Kas",
         "Preis" -> "1"
       )
-      val result = ExtraController.addExtra()(request)
-      status(result) must equalTo(SEE_OTHER)
-      redirectLocation(result) must beSome("/newExtraCreated?id=-1&name=Kas&price=1.0")
       val request2 = FakeRequest(POST, "/selectExtra").withFormUrlEncodedBody(
         "id" -> "0"
       )
-      val result2 = ExtraController.getExtra()(request2)
-      status(result2) must equalTo(SEE_OTHER)
-      redirectLocation(result2) must beSome("/changeExtra?id=0&name=&price=0.0")
+      val result = ExtraController.getExtra()(request2)
+      status(result) must equalTo(SEE_OTHER)
+      redirectLocation(result) must beSome("/changeExtra?id=0&name=&price=0.0")
+    }
+
+    "get an Extra bad request" in memDB {
+      val request = FakeRequest(POST, "/addExtra").withFormUrlEncodedBody(
+        "id" -> "0",
+        "Name" -> "Kas",
+        "Preis" -> "1"
+      )
+      val request2 = FakeRequest(POST, "/selectExtra").withFormUrlEncodedBody(
+        "Id" -> "0"
+      )
+      val result = ExtraController.getExtra()(request2)
+      status(result) must equalTo(BAD_REQUEST)
+      redirectLocation(result) must beNone
+    }
+
+    "show new Extra" in memDB {
+      val request = FakeRequest(POST, "/addExtra").withFormUrlEncodedBody(
+        "id" -> "0",
+        "Name" -> "Kas",
+        "Preis" -> "1"
+      )
+      val result = ExtraController.newExtraCreated(0,"Kas",1)(request)
+      status(result) must equalTo(OK)
+      contentAsString(result) must contain ("0") contain "Kas" contain "1"
+    }
+
+    "show view to change Extra" in memDB {
+      val request = FakeRequest(POST, "/addExtra").withFormUrlEncodedBody(
+        "id" -> "0",
+        "Name" -> "Kas",
+        "Preis" -> "1"
+      )
+      val result = ExtraController.changeExtra1(0,"Speck",2)(request)
+      status(result) must equalTo(OK)
+      contentAsString(result) must contain ("0") contain "Speck" contain "2"
+    }
+
+    "show changed Extra" in memDB {
+      val request = FakeRequest(POST, "/addExtra").withFormUrlEncodedBody(
+        "id" -> "0",
+        "Name" -> "Kas",
+        "Preis" -> "1"
+      )
+      val result = ExtraController.upgradeExtra(0,"Paprika",2)(request)
+      status(result) must equalTo(OK)
+      contentAsString(result) must contain ("0") contain "Paprika" contain "2"
+    }
+
+    "show view for deleted Extra" in memDB {
+      val request = FakeRequest(POST, "/addExtra").withFormUrlEncodedBody(
+        "id" -> "0",
+        "Name" -> "Kas",
+        "Preis" -> "1"
+      )
+      val result = ExtraController.extraDeleted(true)(request)
+      status(result) must equalTo(OK)
+      contentAsString(result) must contain ("Extra Gelöscht!")
     }
   }
 }
