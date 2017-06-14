968bf1450104df5adf10c6f541a327964bb0fbdb
felixthomas <felix.thomas@web.de>
2017-06-14 14:02:23 +0200

    UserControllerSpec auskommentiert

--- test/servicesSpec/OrderServiceSpec.scala
+++ test/servicesSpec/OrderServiceSpec.scala
@@ -1,8 +1,27 @@
 package servicesSpec
 
+import org.junit.runner.RunWith
+import org.specs2.mutable.Specification
+import org.specs2.runner.JUnitRunner
+import play.api.test.FakeApplication
+import play.api.test.Helpers._
+import services.OrderService
+
 /**
   * Created by ifw15124 on 20.04.2017.
   */
-class OrderServiceSpec {
+@RunWith(classOf[JUnitRunner])
+class OrderServiceSpec extends Specification {
+
+  /*def memDB[T](code: => T) =
+    running(FakeApplication(additionalConfiguration = Map(
+      "db.default.driver" -> "org.h2.Driver",
+      "db.default.url" -> "jdbc:h2:mem:test;MODE=PostgreSQL"
+    )))(code)
+
+  "The OrderService should" {
+
+    ""
+  }*/
 
 }
