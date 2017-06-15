c157f49f6abc46152fb7faef413795f9c3f71fc7
ioaccept <frey1@hm.edu>
2017-06-15 21:28:20 +0200

    Rückgänig gemacht

--- app/controllers/OrderController.scala
+++ app/controllers/OrderController.scala
@@ -1,270 +1,155 @@
 package controllers
 
 import play.api.mvc.{Action, AnyContent, Controller}
-import forms._
 import play.api.data.Form
-import play.api.data.Forms._
-import play.api.data.format.Formats._
-import com.github.nscala_time.time.Imports._
+import play.api.data.Forms.{mapping, of, nonEmptyText}
+import services.ExtraService
+import forms.{IDForm, CreateExtraForm}
+import play.api.data.format.Formats.{longFormat, doubleFormat}
 
 /**
-  * Controller for order specific operations.
+  * Controller for extra specific operations.
   *
-  * Import of nscala Library to make time calculations more easily.
-  * (C) https://github.com/nscala-time/nscala-time
+  * GoogleMaps API KEY = AIzaSyC6VCdDfHl2b9yRYnJnMq2PgSjPoXlEXow
   *
   * @author ob, scs
   */
-object OrderController extends Controller {
-val dateFormat = "kk:mm - DD.MM.YYYY"
-  val notavailable = "N/A"
-  val komma = ", "
+object ExtraController extends Controller {
+
+  val success = "success"
 
   /**
-    * Form object for order data.
+    * Form object for extra data.
     */
-  val orderForm = Form(
+  val extraForm = Form(
     mapping(
-      "userID" -> of(doubleFormat),
-      "pizzaID" -> of(doubleFormat),
-      "productID" -> of(doubleFormat),
-      "pizzaAmount" -> default(number, 0),
-      "pizzaSize" -> of(doubleFormat),
-      "productAmount" -> default(number, 0),
-      "extraOneID" -> of(doubleFormat),
-      "extraTwoID" -> of(doubleFormat),
-      "extraThreeID" -> of(doubleFormat))(CreateOrderForm.apply)(CreateOrderForm.unapply))
+      "id" -> of(longFormat),
+      "Name" -> nonEmptyText,
+      "Preis" -> of(doubleFormat))(CreateExtraForm.apply)(CreateExtraForm.unapply))
 
   /**
-    * Form object for modifying order data.
+    * Form object for retrieving extra data from the database.
     */
-  val modifyOrderForm = Form(
+  val selectExtraForm = Form(
     mapping(
-      "orderID" -> of(doubleFormat))(ModifyOrderForm.apply)(ModifyOrderForm.unapply))
+      "id" -> of(longFormat))(IDForm.apply)(IDForm.unapply))
+
 
   /**
-    * Form object for modifying order data.
-    */
-  
-  val orderStatusForm = Form(
-    mapping(
-      "orderID" -> of(longFormat),
-      "orderStatusKZ" -> text)(OrderStatusForm.apply)(OrderStatusForm.unapply))
-
-  /**
-    * Adds a new order with the given data to the system.
+    * Adds a new extra with the given data to the database.
     *
-    * @return page for a new order
+    * @return page for a new extra
     */
-   def createOrder: Action[AnyContent] = Action { implicit request =>
-    orderForm.bindFromRequest.fold(
+  def addExtra: Action[AnyContent] = Action { implicit request =>
+    extraForm.bindFromRequest.fold(
       formWithErrors => {
         BadRequest(views.html.badRequest())
       },
-      orderData => {
-        // try {
-        val selectUser = services.UserService.getUserByID(orderData.userID.toLong)
-        val time: Int = (2 * (selectUser.head.distance.toInt / 1000)) + (10 * orderData.pizzaAmount)
-        var deliveryTime = DateTimeFormat.forPattern(dateFormat).print(DateTime.now() + time.minutes)
-
-        val orderNew = services.OrderService.createOrder(orderData.userID,
-          orderData.pizzaID, orderData.productID, notavailable, notavailable,
-          orderData.pizzaAmount.toDouble, orderData.pizzaSize, -1,
-          orderData.productAmount.toDouble, -1,
-          orderData.extraOneID, notavailable, 0,
-          orderData.extraTwoID, notavailable, 0,
-          orderData.extraThreeID, notavailable, 0,
-          DateTimeFormat.forPattern(dateFormat).print(DateTime.now()), notavailable, deliveryTime)
-        val extraTotalPrice = orderNew.extraOnePrice +
-          orderNew.extraTwoPrice + orderNew.extraThreePrice
-        val extrasName =
-          if (orderNew.extraOneID != 0 && orderNew.extraTwoID == 0
-            && orderNew.extraThreeID == 0) {orderNew.extraOneName
-          } else if (orderNew.extraOneID != 0 && orderNew.extraTwoID != 0
-            && orderNew.extraThreeID == 0) {
-            orderNew.extraOneName + komma + orderNew.extraTwoName
-          } else if (orderNew.extraOneID != 0 && orderNew.extraTwoID != 0
-            && orderNew.extraThreeID != 0) {
-            orderNew.extraOneName + komma +
-              orderNew.extraTwoName + komma + orderNew.extraThreeName
-          }
-        Redirect(routes.OrderController.newOrderCreated(orderNew.id, orderNew.customerID,
-          orderNew.pizzaID, orderNew.productID, orderNew.pizzaName, orderNew.productName,
-          orderNew.pizzaAmount, orderNew.pizzaSize, orderNew.pizzaPrice,
-          orderNew.productAmount, orderNew.productPrice,
-          extrasName.toString, extraTotalPrice, orderNew.totalPrice,
-          orderNew.orderTime, orderNew.status, deliveryTime)).
-          flashing("success" -> "Order saved!")
-        // } catch {
-        // case e: RuntimeException => BadRequest(views.html.orderFailed())
-      }
-    )
-  }
-
-  /**
-    * Updates the status of an order in the database.
-    *
-    * @return page showing all orders
-    */
-  def setStatusOrder: Action[AnyContent] = Action { implicit request =>
-    orderStatusForm.bindFromRequest.fold(
-      formWithErrors => {
-        BadRequest(views.html.orderFailed())
-      },
-      orderData => {
-        // try {
-        val newOrder = services.OrderService.orderSetStaus(orderData.orderID,
-          orderData.orderStatusKZ)
-        Redirect(routes.EmployeeController.showAllOrderDetails()).
-          flashing("success" -> "Order saved!")
-        // } catch {
-        // case e: RuntimeException => BadRequest(views.html.orderFailed())
-      }
-    )
-  }
- 
-  /**
-    * Retrieves an order from the database.
-    *
-    * @return page for a retrieved order
-    */
- def showSelectedOrder: Action[AnyContent] = Action { implicit request =>
-    modifyOrderForm.bindFromRequest.fold(
-      formWithErrors => {
-        BadRequest(views.html.badRequest())
-      },
-      modifyOrderData => {
-        val newOrder = services.OrderService.getOrderbyID(modifyOrderData.orderID)
-        val extraTotalPrice = newOrder.extraOnePrice +
-          newOrder.extraTwoPrice + newOrder.extraThreePrice
-        val extrasName =
-
-          if (newOrder.extraOneID == 0 && newOrder.extraTwoID == 0
-            && newOrder.extraThreeID == 0) {
-            "empty"
-          } else if (newOrder.extraOneID != 0 && newOrder.extraTwoID == 0
-            && newOrder.extraThreeID == 0) {
-            newOrder.extraOneName
-          } else if (newOrder.extraOneID != 0 && newOrder.extraTwoID != 0
-            && newOrder.extraThreeID == 0) {
-            newOrder.extraOneName + komma + newOrder.extraTwoName
-          } else if (newOrder.extraOneID != 0 && newOrder.extraTwoID != 0
-            && newOrder.extraThreeID != 0) {
-            newOrder.extraOneName + komma + newOrder.extraTwoName + komma + newOrder.extraThreeName
-          }
-        Redirect(routes.OrderController.showOrder(newOrder.customerID, newOrder.pizzaID,
-          newOrder.productID, newOrder.pizzaName, newOrder.productName,
-          newOrder.pizzaAmount, newOrder.pizzaSize, newOrder.pizzaPrice,
-          newOrder.productAmount, newOrder.productPrice, newOrder.totalPrice,
-          newOrder.orderTime, newOrder.status, extrasName.toString, extraTotalPrice)).
-          flashing("success" -> "Order saved!")
+      extraData => {
+        val newExtra = services.ExtraService.addExtra(extraData.name, extraData.price)
+        Redirect(routes.ExtraController.newExtraCreated(newExtra.id, newExtra.name, newExtra.price)).
+          flashing(success -> "Extra saved!")
       })
   }
 
   /**
-    * Removes an order from the database.
+    * Changes an already existing extra in the database.
     *
-    * @return page for a deleted order
+    * @return page for a changed extra
     */
- def deleteOrder: Action[AnyContent] = Action { implicit request =>
-    modifyOrderForm.bindFromRequest.fold(
+  def updateExtra: Action[AnyContent] = Action { implicit request =>
+    extraForm.bindFromRequest.fold(
       formWithErrors => {
         BadRequest(views.html.badRequest())
       },
-      modifyOrderData => {
-        val newOrder = services.OrderService.rmOrder(modifyOrderData.orderID)
-        Redirect(routes.OrderController.orderDeleted()).
-          flashing("success" -> "Order saved!")
+      extraData => {
+        val selectExtra = services.ExtraService.updateExtra(extraData.id, extraData.name, extraData.price)
+        Redirect(routes.ExtraController.upgradeExtra(selectExtra.id, selectExtra.name, selectExtra.price)).
+          flashing(success -> "Extra updated!")
       })
   }
 
   /**
-    * Deactivates an order in the database.
+    * Removes/deactivates an extra.
     *
-    * @return page for a deactivated order
+    * @return page for a deleted extra
     */
-def deactivateOrder: Action[AnyContent] = Action { implicit request =>
-    modifyOrderForm.bindFromRequest.fold(
+  def rmExtra: Action[AnyContent] = Action { implicit request =>
+    selectExtraForm.bindFromRequest.fold(
       formWithErrors => {
         BadRequest(views.html.badRequest())
       },
-      modifyOrderData => {
-        val newOrder = services.OrderService.deactivateOrder(modifyOrderData.orderID)
-        Redirect(routes.OrderController.orderDeleted()).
-          flashing("success" -> "Order saved!")
+      deleteExtraData => {
+        val deleteExtraVal = services.ExtraService.rmExtra(deleteExtraData.id)
+        Redirect(routes.ExtraController.extraDeleted(deleteExtraVal)).
+          flashing(success -> "Extra deleted!")
       })
   }
 
   /**
-    * Shows the view for a newly created order.
+    * Retrieves an extra from the database.
     *
-    * @param id              the id of an order
-    * @param customerID      the id of a customer
-    * @param pizzaID         the id of a pizza
-    * @param productID       the id of a product
-    * @param pizzaName       the name of a pizza
-    * @param productName     the name of a product
-    * @param pizzaAmount     the ordered quantity of a pizza
-    * @param pizzaSize       the size of a pizza
-    * @param pizzaPrice      the price of a pizza
-    * @param productAmount   the ordered quantity of a product
-    * @param productPrice    the price of a product
-    * @param extrasName      the name of the extra/s
-    * @param extraTotalPrice the total price of the extra/s
-    * @param totalPrice      the total price of an order
-    * @param orderTime       the time an order has been placed
-    * @param status          the status of an order
-    * @param deliveryTime    the time an order will be delivered
+    * @return page to change an extra
     */
- def newOrderCreated(id: Long, customerID: Double, pizzaID: Double, productID: Double,
-                      pizzaName: String, productName: String,
-                      pizzaAmount: Double, pizzaSize: Double,
-                      pizzaPrice: Double, productAmount: Double, productPrice: Double,
-                      extrasName: Any, extraTotalPrice: Double, totalPrice: Double,
-                      orderTime: String, status: String,
-                      deliveryTime: String): Action[AnyContent] = Action {
-    Ok(views.html.newOrder(id.toInt, customerID.toInt,
-      pizzaID.toInt, productID.toInt, pizzaName, productName,
-      pizzaAmount, pizzaSize, pizzaPrice, productAmount,
-      productPrice, extrasName.toString, extraTotalPrice,
-      totalPrice, orderTime, status, deliveryTime))
+  def getExtra: Action[AnyContent] = Action { implicit request =>
+    selectExtraForm.bindFromRequest.fold(
+      formWithErrors => {
+        BadRequest(views.html.badRequest())
+      },
+      selectExtraData => {
+        val selectExtra = services.ExtraService.selectExtra(selectExtraData.id)
+        Redirect(routes.ExtraController.changeExtra1(selectExtra.id, selectExtra.name, selectExtra.price)).
+          flashing(success -> "Extras retrieved!")
+      })
+  }
+
+
+  /**
+    * Shows the view for a newly created extra.
+    *
+    * @param id    the id of an extra
+    * @param name  the name of an extra
+    * @param price the price of an extra
+    */
+  def newExtraCreated(id: Long, name: String, price: Double): Action[AnyContent] = Action {
+    Ok(views.html.newExtraCreated(id, name, price))
   }
 
   /**
-    * Shows the view for a retrieved order.
+    * Shows the view to change an extra.
     *
-    * @param customerID       the id of a customer
-    * @param pizzaID          the id of a pizza
-    * @param productID        the id of a product
-    * @param pizzaName        the name of a pizza
-    * @param productName      the name of a product
-    * @param pizzaAmount      the ordered quantity of a pizza
-    * @param pizzaSize        the size of a pizza
-    * @param pizzaPrice       the price of a pizza
-    * @param productAmount    the ordered quantity of a product
-    * @param productPrice     the price of a product
-    * @param totalPrice       the total price of an order
-    * @param orderTime        the time an order has been placed
-    * @param status           the status of an order
-    * @param extrasName       the name of the extra/s
-    * @param extrasTotalPrice the total price of the extra/s
+    * @param id    the id of an extra
+    * @param name  the name of an extra
+    * @param price the price of an extra
     */
-  def showOrder(customerID: Double, pizzaID: Double, productID: Double,
-                pizzaName: String, productName: String, pizzaAmount: Double, pizzaSize: Double,
-                pizzaPrice: Double, productAmount: Double, productPrice: Double, totalPrice: Double,
-                orderTime: String, status: String, extrasName: String,
-                extrasTotalPrice: Double): Action[AnyContent] = Action {
-
-    Ok(views.html.showOrder(customerID, pizzaID, productID,
-      pizzaName, productName, pizzaAmount, pizzaSize, pizzaPrice,
-      productAmount, productPrice, totalPrice, orderTime,
-      status, extrasName, extrasTotalPrice))
+  def changeExtra1(id: Long, name: String, price: Double): Action[AnyContent] = Action {
+    Ok(views.html.changeExtra(id, name, price, extraForm))
   }
 
   /**
-    * Shows the view for a deleted order.
+    * Shows the view for a changed extra.
+    *
+    * @param id    the id of an extra
+    * @param name  the name of an extra
+    * @param price the price of an extra
     */
-  def orderDeleted: Action[AnyContent] = Action {
-    Ok(views.html.orderDeleted())
+  def upgradeExtra(id: Long, name: String, price: Double): Action[AnyContent] = Action {
+    Ok(views.html.extraUpdated(id, name, price))
+  }
+
+  /**
+    * Shows the view for a deleted extra.
+    *
+    * @param deleted shows whether or not an extra has been deleted
+    */
+  def extraDeleted(deleted: Boolean): Action[AnyContent] = Action {
+    Ok(views.html.extraDeleted())
+  }
+
+  /**
+    * List all extras currently available in the database.
+    */
+  def showExtras: Action[AnyContent] = Action {
+    Ok(views.html.allExtras(ExtraService.availableExtras))
   }
 }
